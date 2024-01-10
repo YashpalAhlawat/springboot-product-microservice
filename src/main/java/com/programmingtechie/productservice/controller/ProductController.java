417ec1d8119023effc5ec9f306bd9cf1044c6a86
Yashpal Ahlawat <yashpalahlawat8@gmail.com>
2024-01-10 07:03:49 +0530

    Initial Commit

---
+++ src/main/java/com/programmingtechie/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.programmingtechie.productservice.controller;
+
+import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.dto.ProductResponse;
+import com.programmingtechie.productservice.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@AllArgsConstructor // for constructor injection
+public class ProductController {
+    private final ProductService productService;
+    // we need to inject product service in product controller as all logic in that
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
